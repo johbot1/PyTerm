50087ae7af61ce105bc711ca7544fe5dee9c1a82
jbotonakis <jbotonakis@carroll.edu>
2024-10-11 12:40:26 -0600

    Begin core game loop (Also fix syntax on print statements)

--- Hangman/Game.py
+++ Hangman/Game.py
@@ -12,12 +12,18 @@ import Helpers
 os.system("Clear")
 
 #Intialize the Game
-
 active_word = Helpers.wordbank[random.randint(0, len(Helpers.wordbank) - 1)]
-guess = len(active_word)
-hanged_state = hm[0]
+guess_field = len(active_word)
+hanged_state = 0
 
-name = input("Enter your name: ")
+#Name Input With Validation
+while True:
+    name = input("Enter your name: ")
+    if name.isalpha():
+        break
+    else:
+        print("Whoops! Please enter a valid name using only the alphabet; No numbers.")
+
 print(f"Welcome to Hangman {name.capitalize()}!")
 rulecheck = int(input("Do you know the rules of Hangman? 1) Yes 2) No  "))
 while rulecheck != 1:
@@ -31,17 +37,23 @@ while rulecheck != 1:
 print("Great!")
 input("Press Enter to begin...")
 playing = True
-# Print the initial state of the hangman and the word in underscores
-print(hanged_state)
-
-# Create and display the underscores for the word to be guessed
-word_display = "_ " * len(active_word)
-print(word_display.strip())
-
-playerguess = input(f"GUESS: ")
 
 
 #MAIN LOOP
-# while true:
-#     print (f"Current Guess: {guess}")
-#     print("Enter a ")
+while playing:
+    # Print the initial state of the hangman and the word in underscores
+    print(hanged_state)
+
+    # Create and display the underscores for the word to be guessed
+    word_display = "_ " * guess_field
+    print(word_display.strip())
+
+    #Player input validation (Only letters, nothing else)
+    while True:
+        playerguess = input(f"GUESS: ")
+        if len(playerguess) == 1 and playerguess.isalpha():
+            break
+        else:
+            print("Whoops! Please enter a valid guess. That's one letter at a time!")
+    if playerguess not in active_word:
+        hanged_state + 1
--- Hangman/Helpers.py
+++ Hangman/Helpers.py
@@ -11,11 +11,11 @@ wordbank = ['Macintosh', 'Linux', 'Windows', 'Malware', 'Python', 'Java', 'Techn
 def rules():
     print("No problem! Here's how to play...")
     time.sleep(2)
-    print("The object of this game is to guess a word letter by letter")
+    print("The object of this game is to guess a word letter by letter.")
     time.sleep(2)
     print("Every wrong guess will draw another piece onto the gallows!")
     time.sleep(2)
-    print("If you miss 5 guesses in a row, you lose and the man is hung")
+    print("If you miss 5 guesses in a row, you lose and the man is hung.")
     time.sleep(2)
     print("Solve the word before running out of guesses and you win!")
     time.sleep(2)
